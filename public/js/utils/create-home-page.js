18aafb0d41ed6add203bec7523af95761faf8bb4
Bryan Lazo <lebry4n@gmail.com>
2023-04-08 22:29:59 -0600

    Adding is alive indicator

--- public/js/utils/create-home-page.js
+++ public/js/utils/create-home-page.js
@@ -17,16 +17,30 @@ export const createHomePage = () => __awaiter(void 0, void 0, void 0, function* 
     const { content } = cardTemplate;
     container.innerHTML = "";
     results.forEach((character) => {
-        const { name, image, species, status } = character;
+        const { name, image, species, status, location, origin, url, gender } = character;
         const card = content.cloneNode(true);
         const cardImage = card.querySelector("[data-image]");
         const cardName = card.querySelector("[data-name]");
         const cardSpecies = card.querySelector("[data-species]");
         const cardStatus = card.querySelector("[data-status]");
+        const cardGender = card.querySelector("[data-gender]");
+        const cardLocation = card.querySelector("[data-location]");
+        const cardOrigin = card.querySelector("[data-origin]");
+        const cardIndicator = card.querySelector("[data-indicator]");
         cardImage.src = image;
         cardName.textContent = name;
+        cardName.href = url;
         cardSpecies.textContent = species;
+        cardGender.textContent = gender;
         cardStatus.textContent = status;
+        cardLocation.textContent = location.name;
+        cardOrigin.textContent = origin.name;
+        const statusColor = {
+            Alive: "card__indicator--alive",
+            Dead: "card__indicator--dead",
+            unknown: "card__indicator--unknown",
+        };
+        cardIndicator.classList.add(statusColor[status]);
         cardContainer.appendChild(card);
     });
     container.appendChild(cardContainer);
--- src/ts/utils/create-home-page.ts
+++ src/ts/utils/create-home-page.ts
@@ -12,21 +12,40 @@ export const createHomePage = async () => {
   container.innerHTML = "";
 
   results.forEach((character: Character) => {
-    const { name, image, species, status } = character;
+    const { name, image, species, status, location, origin, url, gender } =
+      character;
     const card = content.cloneNode(true) as HTMLElement;
     const cardImage = card.querySelector("[data-image]") as HTMLImageElement;
-    const cardName = card.querySelector("[data-name]") as HTMLHeadingElement;
-    const cardSpecies = card.querySelector(
-      "[data-species]"
+    const cardName = card.querySelector("[data-name]") as HTMLAnchorElement;
+    const cardSpecies = card.querySelector("[data-species]") as HTMLSpanElement;
+    const cardStatus = card.querySelector("[data-status]") as HTMLSpanElement;
+    const cardGender = card.querySelector("[data-gender]") as HTMLSpanElement;
+    const cardLocation = card.querySelector(
+      "[data-location]"
     ) as HTMLParagraphElement;
-    const cardStatus = card.querySelector(
-      "[data-status]"
+    const cardOrigin = card.querySelector(
+      "[data-origin]"
     ) as HTMLParagraphElement;
+    const cardIndicator = card.querySelector(
+      "[data-indicator]"
+    ) as HTMLSpanElement;
 
     cardImage.src = image;
     cardName.textContent = name;
+    cardName.href = url;
     cardSpecies.textContent = species;
+    cardGender.textContent = gender;
     cardStatus.textContent = status;
+    cardLocation.textContent = location.name;
+    cardOrigin.textContent = origin.name;
+
+    const statusColor = {
+      Alive: "card__indicator--alive",
+      Dead: "card__indicator--dead",
+      unknown: "card__indicator--unknown",
+    };
+
+    cardIndicator.classList.add(statusColor[status]);
 
     cardContainer.appendChild(card);
   });
